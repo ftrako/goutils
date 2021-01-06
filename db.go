63067dc6df286174835b8e54080d3af55a6b9d2c
chendajian <chendajian@yahoo.com>
2021-01-06 10:59:21 +0800

    fix pg

--- db.go
+++ db.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -13,6 +14,18 @@ func NewMysqlDB(host string, port int, user, pwd, dbName string, maxIdle int) (*
 	str := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8", user, pwd, host, port, dbName)
 
 	db := sqlx.MustOpen("mysql", str)
+	if err := db.Ping(); err != nil { // 检查连接性
+		return nil, errors.New(fmt.Sprintf("%v %v", err, str))
+	}
+	db.SetMaxIdleConns(maxIdle)
+	return db, nil
+}
+
+func NewPGDB(host string, port int, user, pwd, dbName string, maxIdle int) (*sqlx.DB, error) {
+	str := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, pwd, dbName)
+
+	db := sqlx.MustOpen("postgres", str)
 	if err := db.Ping(); err != nil { // 检查连接性
 		return nil, errors.New(fmt.Sprintf("%v %v", err, str))
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/json-iterator/go v1.1.10
+	github.com/lib/pq v1.0.0
 	github.com/satori/go.uuid v1.2.0
 	golang.org/x/text v0.3.4
 )
